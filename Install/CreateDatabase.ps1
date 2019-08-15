7f4fdbae03cfa3018a08249ee13f45af85ccfde7
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-15 15:53:24 -0400

    SQL script to create database

---
+++ Install/CreateDatabase.ps1
@@ -0,0 +1,35 @@
+#Requires -Modules SqlServer
+#Requires -Version 5
+
+<#
+    .SYNOPSIS
+    Creates a new Database via PowerShell
+
+    .DESCRIPTION
+
+
+    .PARAMETER SqlServerInstance
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+
+param (
+    [string]$SqlServerInstance,
+    [string]$DatabaseName = "WebAppMonitor"
+
+)
