f31ce2021f770fc52675e701ade1144c45056e4f
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-20 18:11:38 -0400

    basic cmdlet that creates the database. need to add more sql statements for creating the tables along with retreiving sql modules and error handling to deal with them not being present on the system

---
+++ Module/WAM/Install-WAMDatabase.ps1
@@ -0,0 +1,89 @@
+#requires -Version 4.0
+function Install-WAMDatabase {
+<#
+    .SYNOPSIS
+    Creates the database for WebAppMonitor.
+
+    .DESCRIPTION
+    Executes SQL Queries to setup the database. This cmdlet requires either
+    SqlServer or SQLPS.
+
+    .PARAMETER ServerInstance
+    Default value is is localhost
+
+    .PARAMETER DatabaseName
+    Name of the SQL Database to be created. The default value is WebAppMonitor.
+
+    .PARAMETER Credential
+    SQL Login to connect to the Database Engine to create the Database.
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+    Initial
+    8/19/2019
+    Brian Sexton
+
+#>
+    [CmdletBinding()]
+    param(
+
+        [Parameter(Mandatory)]
+        [string]$ServerInstance,
+
+        [Parameter()]
+        [string]$DatabaseName = "WebAppMonitor",
+
+        [Parameter()]
+        [pscredential]$Credential
+    )
+
+    begin {
+
+        $SQLServerModules = Get-Module -ListAvailable | Where-Object {$_.Name -eq 'SQLPS' -or $_.Name -eq 'SqlServer'}
+
+    }
+    process {
+
+        $SQLQuery = "CREATE DATABASE $DatabaseName"
+
+        if ($PSBoundParameters.ContainsKey($Credential))
+        {
+
+            try {
+                Write-Verbose "Attempting to Create Database $DatabaseName on $ServerInstance"
+                Invoke-Sqlcmd -ServerInstance $ServerInstance -Query $SQLQuery -Credential $Credential -ErrorAction Stop
+            }
+            catch {
+                Write-Host "Failed to Create Database" -ForegroundColor Red
+                $Error[0]
+            }
+        }
+        else
+        {
+
+            try {
+                Write-Verbose "Attempting to Create Database $DatabaseName on $ServerInstance"
+                Invoke-Sqlcmd -ServerInstance $ServerInstance -Query $SQLQuery -ErrorAction Stop
+            }
+            catch {
+                Write-Host "Failed to Create Database" -ForegroundColor Red
+                $Error[0]
+            }
+        }
+
+    }
+    end {
+
+    }
+}
