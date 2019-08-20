15d568b0fd35b16b88f27d956d90cb973f065643
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-20 18:15:52 -0400

    started working on the cmdlet which will add sites or apps to be monitored to the database

---
+++ Module/WAM/Add-WAMAppUri.ps1
@@ -0,0 +1,65 @@
+function Add-WAMAppUri {
+<#
+    .SYNOPSIS
+
+
+    .DESCRIPTION
+
+
+    .PARAMETER Uri
+    The URI to monitor.
+
+    .PARAMETER StatusCode
+    The HTTP status code that should be returned.
+
+    .PARAMETER DatabaseInstance
+    The Instance of SQL Server where the database is located.
+
+    .PARAMETER Credential
+    A SQL Login to connect to the database engine and write information.
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>  [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory)]
+        [string]$Uri,
+
+
+
+        [Parameter(Mandatory)]
+        [int]$StatusCode,
+
+        [Parameter(Mandatory)]
+        [string]$DatabaseInstance,
+
+        [Parameter()]
+        [pscredential]$Credential
+    )
+
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+
+}
