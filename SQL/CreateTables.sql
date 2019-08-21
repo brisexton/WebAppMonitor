5b16828612bf004aef2b9b5c52ae3cab3efbc2d3
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-21 14:11:17 -0400

    started to work on sql script to generate all of the tables

---
+++ SQL/CreateTables.sql
@@ -0,0 +1,21 @@
+USE [WebAppMontior]
+GO
+
+
+CREATE TABLE [dbo].[webapps](
+	[webapp_id] [int] IDENTITY(1,1) NOT NULL,
+    [name] [nvarchar(50)] NOT NULL,
+    [description] [nvarchar(160)] NULL,
+	[uri] [nvarchar(160)] NOT NULL
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[headers](
+    [webapp_id] [int] NOT NULL,
+    [header_key] [nvarchar(100)] NULL,
+    [header_value] [nvarchar(100)] NULL
+)
+
+CREATE TABLE [dbo].[apptests](
+
+)
