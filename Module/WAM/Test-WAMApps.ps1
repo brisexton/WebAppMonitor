8a537968268ddc5a3573be99f26cbeaf5ed0b1fa
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-21 14:13:55 -0400

    placeholder for executing the tests stored  in the database

---
+++ Module/WAM/Test-WAMApps.ps1
@@ -0,0 +1,41 @@
+function Test-WAMApps.ps1 {
+<#
+    .SYNOPSIS
+    Runs tests against the URL's provided.
+
+    .DESCRIPTION
+
+
+    .PARAMETER
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+    }
+
+    process {
+    }
+
+    end {
+    }
+}
