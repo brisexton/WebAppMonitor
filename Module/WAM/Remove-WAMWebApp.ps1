9a2b993e3487d9f15260a1452f1ebe8af14284a1
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-28 18:10:55 -0400

    added placeholder for cmdlet to remove an app from the database

---
+++ Module/WAM/Remove-WAMWebApp.ps1
@@ -0,0 +1,33 @@
+function Remove-WAMWebApp {
+<#
+    .SYNOPSIS
+    Removes a site from the database
+
+    .DESCRIPTION
+
+
+    .PARAMETER
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+    [CmdletBinding()]
+    param(
+
+
+    )
+}
