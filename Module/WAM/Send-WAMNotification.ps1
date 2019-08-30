723e17dfadc730d52a0447484dcfbf78cd50de5f
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-30 10:37:15 -0400

    renamed file and updated cmdlet name

---
+++ Module/WAM/Send-WAMNotification.ps1
@@ -0,0 +1,46 @@
+function Send-WAMNotification {
+
+<#
+    .SYNOPSIS
+    Send's Notifications for Tests performed.
+
+    .DESCRIPTION
+
+
+    .PARAMETER
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+    [CmdletBinding()]
+    param(
+
+        [Parameter(Mandatory)]
+        [ValidateSet('PROD','TEST','DEV')]
+        [string]
+    )
+
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+}
