af7207f85e1e82890479e5fe17194d5f837d9b92
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-30 10:21:22 -0400

    added additional cmdlets to script module

--- Module/WAM/WAM.psm1
+++ Module/WAM/WAM.psm1
@@ -8,5 +8,13 @@
 
 #>
 
-. $PSScriptRoot\Register-WAMScheduledTask.ps1
+
 . $PSScriptRoot\Install-WAMDatabase.ps1
+. $PSScriptRoot\Get-WAMWebApp.ps1
+. $PSScriptRoot\New-WAMWebApp.ps1
+. $PSScriptRoot\Set-WAMWebApp.ps1
+. $PSScriptRoot\Remove-WAMWebApp.ps1
+. $PSScriptRoot\Test-WAMApps.ps1
+. $PSScriptRoot\Update-WAMLog.ps1
+. $PSScriptRoot\Send-WAMEmailNotification.ps1
+. $PSScriptRoot\Register-WAMScheduledTask.ps1
