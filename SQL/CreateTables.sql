07ce19cf1e3bd900f6c7cd5ac88ca2cdce0e8bc2
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-30 15:04:43 -0400

    added email config table and had to fix brackets, thanks to vs code intellisense, fixed sppelling issues

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -1,20 +1,21 @@
-USE [WebAppMontior]
+USE [WebAppMonitor]
 GO
 
 
 CREATE TABLE [dbo].[webapps](
 	[webapp_id] [int] IDENTITY(1,1) NOT NULL,
-    [name] [nvarchar(50)] NOT NULL,
-    [description] [nvarchar(160)] NULL,
-	[uri] [nvarchar(160)] NOT NULL
+    [name] [nvarchar](50) NOT NULL,
+    [description] [nvarchar](160) NULL,
+	[uri] [nvarchar](160) NOT NULL
+
 ) ON [PRIMARY]
 GO
 
 CREATE TABLE [dbo].[headers](
     [webapp_id] [int] NOT NULL,
     [order] [int] NULL,
-    [header_key] [nvarchar(100)] NULL,
-    [header_value] [nvarchar(100)] NULL
+    [header_key] [nvarchar](100) NULL,
+    [header_value] [nvarchar](100) NULL
 )
 GO
 
@@ -23,26 +24,30 @@ CREATE TABLE [dbo].[apptests](
     [test_id] [int] NOT NULL,
     [webapp_id] [int] NOT NULL,
     [status_code] [int] NOT NULL,
+    [test_time] [datetime] NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[notify_type](
     [notifytype_id] [int] NOT NULL,
-    [name] [nvarchar(10)] NOT NULL,
-    [description] [nvarchar(100)] NULL
+    [name] [nvarchar](10) NOT NULL,
+    [description] [nvarchar](100) NULL
 )
 GO
 
 CREATE TABLE [dbo].[notification](
+    [notification_id] [int] IDENTITY(1,1) NOT NULL,
     [webapp_id] [int] NOT NULL,
-    [notification_type] [nvarchar(100)] NULL,
-    [notification_message] [nvarchar(100)] NULL
-)
+    [notification_type] [nvarchar](100) NULL,
+    [notification_message] [nvarchar](max) NULL
+) ON [PRIMARY]
 GO
 
 CREATE TABLE [dbo].[emailconfig](
     [emailsettings_id] [int] NOT NULL,
-    [port] [int] NULL,
-    [header_value] [nvarchar(100)] NULL
+    [from_address] [nvarchar](50) NOT NULL,
+    [servername] [nvarchar](100) NOT NULL,
+    [port] [int] NOT NULL,
+    [usessl] [bit] NOT NULL
 )
 GO
