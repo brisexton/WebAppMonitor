1797ca0220f5c42f8815e914593da556cf064bf9
Brian Sexton <37346284+brisexton@users.noreply.github.com>
2019-08-30 21:12:46 -0400

    changed script to drop the database if exists then to create it

--- SQL/CreateDatabase.sql
+++ SQL/CreateDatabase.sql
@@ -1,2 +1,16 @@
+/* Drop database if exists and then create */
+
+USE [master]
+GO
+
+ALTER DATABASE [WebAppMonitor] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
+GO
+
+USE [master]
+GO
+
+DROP DATABASE [WebAppMonitor]
+GO
+
 CREATE DATABASE WebAppMonitor
 GO
