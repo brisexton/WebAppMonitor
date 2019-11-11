8ed534ddfd33b308fa9161e7cfe8b6f5759be294
Brian Sexton <bsexton@dynamicbatech.com>
2019-11-10 22:26:08 -0500

    added scaffold and parameters

---
+++ Module/WAM/Get-WAMWebApp.ps1
@@ -0,0 +1,49 @@
+function Get-WAMWebApp {
+
+    <#
+    .SYNOPSIS
+
+
+    .DESCRIPTION
+
+
+    .PARAMETER
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+    [CmdletBinding()]
+    param(
+
+        [Parameter()]
+        [string]$Name,
+
+        [Parameter()]
+        [switch]$Active
+
+    )
+
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+}
