eb867a76fbed685236ce79ce656b3594963d0662
Brian Sexton <bsexton@dynamicbatech.com>
2019-11-10 22:33:08 -0500

    updated help

--- Module/WAM/Get-WAMWebApp.ps1
+++ Module/WAM/Get-WAMWebApp.ps1
@@ -1,13 +1,28 @@
+#Requires -Module SqlServer
 function Get-WAMWebApp {
-
     <#
     .SYNOPSIS
-
+    Retrieve's Web Applications in the database
 
     .DESCRIPTION
 
 
-    .PARAMETER
+    .PARAMETER ServerInstance
+    The SQL Server Instance where the database lives. Default is
+    the default instance on localhost.
+
+    .PARAMETER DatabaseName
+    The name of the WebAppMonitor database, if changed from the
+    default name of WebAppMonitor.
+
+    .PARAMETER Name
+    The name of the Web Application that has been registered.
+
+    .PARAMETER Active
+    Whether the App is set to be actively monitored.
+
+    .PARAMETER Credential
+    SQL Credential to connect to the SQL Server Instance.
 
 
     .EXAMPLE
@@ -23,17 +38,28 @@ function Get-WAMWebApp {
 
 
     .NOTES
-
+    Initial
+    11/10/2019
+    Brian Sexton
 
 #>
     [CmdletBinding()]
     param(
 
         [Parameter()]
+        [string]$ServerInstance,
+
+        [Parameter()]
+        [string]$DatabaseName = 'WebAppMonitor',
+
+        [Parameter()]
         [string]$Name,
 
         [Parameter()]
-        [switch]$Active
+        [switch]$Active,
+
+        [parameter()]
+        [pscredential]$Credential
 
     )
 
