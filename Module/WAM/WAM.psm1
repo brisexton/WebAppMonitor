e95ada0ddebd8160936c3ebea5f3196eb2cf8339
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-16 15:23:13 -0500

    updated script files to be imported for module and added comments for each section

--- Module/WAM/WAM.psm1
+++ Module/WAM/WAM.psm1
@@ -9,24 +9,33 @@
 
 #>
 
-
+# Creates the database and tables.
 . $PSScriptRoot\Install-WAMDatabase.ps1
+
+# Adds a web application, to be monitored, to the database.
 . $PSScriptRoot\Get-WAMWebApp.ps1
 . $PSScriptRoot\New-WAMWebApp.ps1
 . $PSScriptRoot\Set-WAMWebApp.ps1
 . $PSScriptRoot\Remove-WAMWebApp.ps1
-. $PSScriptRoot\Test-WAMApps.ps1
-. $PSScriptRoot\Update-WAMLog.ps1
 
-. $PSScriptRoot\Register-WAMScheduledTask.
+# Scheduled Task
+. $PSScriptRoot\Install-WAMScheduledTask.ps1
+. $PSScriptRoot\Uninstall-WAMScheduledTask.ps1
 
+# Configure Notification/Alerting System
 . $PSScriptRoot\New-WAMNotificationSystem.ps1
 . $PSScriptRoot\Get-WAMNotificationSystem.ps1
 . $PSScriptRoot\Set-WAMNotificationSystem.ps1
 . $PSScriptRoot\Remove-WAMNotificationSystem.ps1
 
+# Configure Notification/Alert Recipients
 . $PSScriptRoot\New-WAMNotification.ps1
 . $PSScriptRoot\Set-WAMNotification.ps1
 . $PSScriptRoot\Get-WAMNotification.ps1
 . $PSScriptRoot\Remove-WAMNotification.ps1
+. $PSScriptRoot\Test-WAMNotification.ps1
+
+# Used during routine tests
+. $PSScriptRoot\Test-WAMApps.ps1
+. $PSScriptRoot\Update-WAMLog.ps1
 . $PSScriptRoot\Send-WAMNotification.ps1
