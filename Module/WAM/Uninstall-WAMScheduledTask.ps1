be9248f2e1f8c1f106191a2130c18e97391a7eb4
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-16 15:18:32 -0500

    cmdlet to remove the scheduled task from Task Scheduler

---
+++ Module/WAM/Uninstall-WAMScheduledTask.ps1
@@ -0,0 +1,58 @@
+#Requires -RunAsAdministrator
+function Uninstall-WAMScheduledTask {
+
+    <#
+    .SYNOPSIS
+    Removes scheduled task from Task Scheduler.
+
+    .DESCRIPTION
+
+
+    .PARAMETER TaskName
+    Name of scheduled task.
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+    Initial
+    12/16/2019
+    Brian Sexton
+
+#>
+    [CmdletBinding()]
+    param(
+
+        [Parameter()]
+        [string]$TaskName = 'WebAppMonitor'
+
+    )
+
+    begin {
+
+    }
+    process {
+
+        try {
+            Write-Verbose -Message "Attempting to find task name $TaskName in Task Scheduler."
+            $null = Get-ScheduledTask -TaskName $TaskName -ErrorAction Stop
+            Write-Verbose -Message "Found Scheduled Task!"
+            Unregister-ScheduledTask -TaskName $TaskName
+        } catch {
+            Write-Error -Message "Couldn't find scheduled task with name $TaskName in Task Scheduler."
+        }
+
+    }
+    end {
+
+    }
+}
