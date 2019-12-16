889fddd94c3671500af0c661222c34dee995ae88
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-16 18:44:14 -0500

    added paramters for the web app name or id and database information

--- Module/WAM/Remove-WAMWebApp.ps1
+++ Module/WAM/Remove-WAMWebApp.ps1
@@ -1,12 +1,26 @@
 function Remove-WAMWebApp {
-<#
+    <#
     .SYNOPSIS
     Removes a site from the database.
 
     .DESCRIPTION
 
 
-    .PARAMETER
+    .PARAMETER Name
+    The friendly name of the web app registered in the database.
+
+    .PARAMETER WebAppID
+    The ID of the web app registered in the database.
+
+    .PARAMETER DatabaseName
+    The name of the database used by WebAppMonitor. The default value is
+    WebAppMonitor.
+
+    .PARAMETER ServerInstance
+    The Instance of SQL Server where the database is located.
+
+    .PARAMETER Credential
+    SQL Login credentials to connect to the SQL Server Instance.
 
 
     .EXAMPLE
@@ -22,12 +36,28 @@ function Remove-WAMWebApp {
 
 
     .NOTES
-
+    Initial
+    12/16/2019
+    Brian Sexton
 
 #>
     [CmdletBinding()]
     param(
 
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "ByName")]
+        [string]$Name,
+
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "ByID")]
+        [int]$WebAppID,
+
+        [Parameter()]
+        [string]$DatabaseName = "WebAppMonitor",
+
+        [Parameter()]
+        [string]$ServerInstance = $env:COMPUTERNAME,
+
+        [Parameter()]
+        [pscredential]$Credential
 
     )
 
