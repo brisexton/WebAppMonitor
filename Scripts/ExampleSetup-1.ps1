e699295bd2b647eabbca3b2c48543ca2a217d5ef
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-16 21:49:08 -0500

    added code to create local user account and create scheduled task as that account

--- Scripts/ExampleSetup-1.ps1
+++ Scripts/ExampleSetup-1.ps1
@@ -10,15 +10,32 @@ Install-WAMDatabase
 # Add Web application to be monitored
 New-WAMWebApp -Name "CNN.com" -Description "Sample website to make sure it's working correctly" -Url "https://www.cnn.com" -StatusCode 200 -Method GET -ServerInstance DEVLT003-VH\DEV
 
-
 # Add notification system for alerts
-
+New-WAMNotificationSystem
 
 
 # add notification address for alerts
 
 
+
+# create local user account to run the scheduled task as
+$RunAsUserName = "WAMUser"
+$RunAsUserPassword = ConvertTo-SecureString -String 'TH1s1snt@S3curePassw0rd' -AsPlainText -Force
+
+$LocalUserInfo = @{
+    Name = $RunAsUserName
+    Description = "RunAs account for WebAppMonitor Scheduled Task for Web Application Monitoring."
+    Password = $RunAsUserPassword
+    AccountNeverExpires = $true
+    ChangePasswordAtNextLogon = $false
+    PasswordNeverExpires = $true
+}
+
+New-LocalUser @LocalUserInfo
+
 # add as scheduled task
-$cred = Get-Credential -Message "Enter Credentials to run scheduled task as"
+
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $RunAsUserName,$RunAsUserPassword
+
 Install-WAMScheduledTask -RuntimeFrequency 120 -InstallLocation C:\scripts -ScriptFilePath C:\scripts\WAMRuntime.ps1 -Credential $cred
 
