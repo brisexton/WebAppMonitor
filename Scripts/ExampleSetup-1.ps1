cb38191f23fd7e4c641bd1b1c3e18924d5ee87b3
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-16 22:37:12 -0500

    added example command to add smtp server as notification/alerting system and SQL Login

--- Scripts/ExampleSetup-1.ps1
+++ Scripts/ExampleSetup-1.ps1
@@ -4,14 +4,36 @@ Set-Location -Path $BasePath
 
 Import-Module .\Module\WAM\WAM.psm1 -Verbose
 
+# variables (if you create these variables, setup will be easier)
+$SQLInstance = "$env:COMPUTERNAME\DEV"
+$SQLLoginUser = "webappmonitor_usr"
+$SQLLoginPassword = ConvertTo-SecureString -String "Isth1sAbadPassw0rd0rWHAT" -AsPlainText -Force
+
 # install database
-Install-WAMDatabase
+Install-WAMDatabase -ServerInstance $SQLInstance
+New-WAMSQLLogin
 
 # Add Web application to be monitored
 New-WAMWebApp -Name "CNN.com" -Description "Sample website to make sure it's working correctly" -Url "https://www.cnn.com" -StatusCode 200 -Method GET -ServerInstance DEVLT003-VH\DEV
 
 # Add notification system for alerts
-New-WAMNotificationSystem
+
+$SMTPServerUserName = 'alerts@dynamicbatech.com'
+$SMTPServerPassword = ConvertTo-SecureString -String "Th3F1stR3@llybadPassw0rd" -AsPlainText -Force
+$SMTPServerCreds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SMTPServerUserName,$SMTPServerPassword
+
+$NotificationSystemInfo = @{
+    Name = "DBAT Email Server"
+    NotificationType = Email
+    FromName = "DBAT Alerts"
+    FromAddress = 'alerts@dynamicbatech.com'
+    SMTPServer = "smtpserver.dynamicbatech.com"
+    SMTPCredential = $SMTPServerCreds
+    Port = 465
+    UseSSL = $true
+}
+
+New-WAMNotificationSystem @NotificationSystemInfo
 
 
 # add notification address for alerts
@@ -19,7 +41,7 @@ New-WAMNotificationSystem
 
 
 # create local user account to run the scheduled task as
-$RunAsUserName = "WAMUser"
+$RunAsUserName = "$env:COMPUTERNAME\WAMUser"
 $RunAsUserPassword = ConvertTo-SecureString -String 'TH1s1snt@S3curePassw0rd' -AsPlainText -Force
 
 $LocalUserInfo = @{
