0d3c2918c4bea1ec8c58a11c6a1356d220a5db67
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-24 02:00:34 -0500

    added some standard parameters with parameter sets and associated help

--- Module/WAM/Set-WAMWebApp.ps1
+++ Module/WAM/Set-WAMWebApp.ps1
@@ -2,12 +2,26 @@ function Set-WAMWebApp {
 
     <#
     .SYNOPSIS
-    Change Values on a monitored Web Application.
+    Change Values associated with a Web Application.
 
     .DESCRIPTION
 
 
-    .PARAMETER
+    .PARAMETER Name
+    The Friendly Name of the web application.
+
+    .PARAMETER WebAppId
+    The ID number associated with the web application in the database.
+
+    .PARAMETER DatabaseName
+    The name of the database used by WebAppMonitor. The default value is
+    WebAppMonitor.
+
+    .PARAMETER ServerInstance
+    The Instance of SQL Server where the database is located.
+
+    .PARAMETER Credential
+    A SQL Login to connect to the database engine and write information.
 
 
     .EXAMPLE
@@ -23,12 +37,33 @@ function Set-WAMWebApp {
 
 
     .NOTES
-
+    Initial
+    12/23/2019
+    Brian Sexton
 
 #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "ByName")]
     param(
 
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "ByName")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "ById")]
+        [ValidateNotNullOrEmpty()]
+        [int]$WebAppId,
+
+
+
+
+        [Parameter()]
+        [string]$DatabaseName = "WebAppMonitor",
+
+        [Parameter()]
+        [string]$ServerInstance = $env:COMPUTERNAME,
+
+        [Parameter()]
+        [pscredential]$Credential
 
     )
 
