c7b05fa22898555e2468b63cbbc5a292ebf8abb1
Brian Sexton <bsexton@dynamicbatech.com>
2019-12-24 01:59:44 -0500

    added scaffold

---
+++ Module/WAM/Set-WAMWebApp.ps1
@@ -0,0 +1,44 @@
+function Set-WAMWebApp {
+
+    <#
+    .SYNOPSIS
+    Change Values on a monitored Web Application.
+
+    .DESCRIPTION
+
+
+    .PARAMETER
+
+
+    .EXAMPLE
+
+
+    .INPUTS
+
+
+    .OUTPUTS
+
+
+    .LINK
+
+
+    .NOTES
+
+
+#>
+    [CmdletBinding()]
+    param(
+
+
+    )
+
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+}
